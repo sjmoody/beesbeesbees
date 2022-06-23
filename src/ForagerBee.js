9e4c88e8189c71a3ea7be28818c611ebc3cbcad3
Steven Moody <smoody07@gmail.com>
2022-06-22 21:27:26 -0700

    Complete Bare Minimum Requirements

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
 };
 
+// set prototype and constructor
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+// forage method
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+}
